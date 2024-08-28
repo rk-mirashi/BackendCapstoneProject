6069255fdf304123181a65283dc937acf9f46932
rk-mirashi <mirashirushikesh@gmail.com>
2024-08-28 13:49:04 +0530

    Created API's

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
 
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('users', views.UserView.as_view()),
+    path('menu/', views.MenuItemsView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from rest_framework import viewsets,generics
+from django.contrib.auth.models import User
+from .models import Menu,Booking
+from .serializers import UserSerializer,MenuSerializer,BookingSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class UserView(generics.ListCreateAPIView):
+    queryset=User.objects.all()
+    serializer_class=UserSerializer
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
